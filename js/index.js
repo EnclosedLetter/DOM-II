c95f7d60fb59a50aa822d4c4ca7d505c4877429e
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-10 15:56:42 -0700

    Got event listeners to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+// console.log("something else still");
+
+
+// //addEventListener works on elements, document and widndow
+// document.querySelector('nav').addEventListener('click',()=>{
+//     console.log('the nav got clicked');
+// })
+
+// BUS PIC 
+const busPic = document.querySelector(".intro").addEventListener("dblclick", function(){
+    document.getElementById("fBus").style.width = "75%";
+})
+
+//BUS TEXT
+const busText = document.querySelector(".nav-container").addEventListener("mouseover", function(){
+    document.querySelector(".logo-heading").textContent = "WELCOME TO THE FUN BUS!";
+})
+
+// LET'S GO Text
+const adventurePic = document.querySelector(".img-content").addEventListener("dblclick", function(){
+    document.querySelector("img").textContent = "BOO!";
+})
+
+// const busPic = document.querySelector(".intro").addEventListener("mouseover", function(){
+//     document.getElementById("fBus").style.width = "50%";
+// })
