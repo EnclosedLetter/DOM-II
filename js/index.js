df2469222dcf8d3b8462aef7c03459d392d2c7b9
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-11 09:05:14 -0700

    done

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 // Your code goes here
 // console.log("something else still");
 
-//BUS TEXT
+//BUS TEXT 1
 const busText = document.querySelector(".nav-container").addEventListener("mouseover", function(){
     document.querySelector(".logo-heading").textContent = "HOP ON THE FUN BUS!";
 })
 
-// BUS PIC 
+// BUS PIC 2
 const busPic = document.querySelector(".intro").addEventListener("dblclick", function(){
     document.getElementById("fBus").style.width = "75%";
 })
 
-// LET'S GO Text
-// const letsgoText = document.querySelector(".text-content").addEventListener("keydown", function(){
+// LET'S GO Text 3
 document.addEventListener("keydown", event => {
     if (event.key === "q"){
         document.body.style.color = "purple"; 
@@ -23,12 +22,12 @@ document.addEventListener("keydown", event => {
 })
 // YOU CAN PUT MULTIPLE TAGS IN QUERY SELECTOR
 
-// ADVENTURE pic
+// ADVENTURE pic 4
 const adventurePic = document.querySelector(".img-content").addEventListener("dblclick", function(){
     document.querySelector("#advPic").style.width = "50%";
 })
 
-// CHANGE BOAT SCALE
+// CHANGE BOAT PIC SCALE 5
 const boatPic = document.querySelector(".content-section .img-fluid-rounded");
 boatPic.addEventListener('mouseenter', ()=>{
     boatPic.style.transform = "scale(1.2)";
@@ -40,21 +39,44 @@ boatPic.addEventListener("mouseleave", () => {
 });
 
 
-// CHANGE BACKGROUND BY PRESSING ANY KEY
+// CHANGE BACKGROUND BY PRESSING ANY KEY 6
 const backgroundClr = document.querySelector("*");
 backgroundClr.addEventListener('keypress', event =>{
     backgroundClr.style.backgroundColor = "orange";
 })
 
-// const otherVar = document.querySelector("*"); THIS CHANGES THE BACKGROUND BACK TO WHITE
+// const otherVar = document.querySelector("*"); THIS CHANGES THE BACKGROUND BACK TO WHITE 7
 document.addEventListener('keydown', event => {
     if (event.key === "w"){
         document.body.style.backgroundColor = "white";
     }
 })
 
+// CHANGED TO DOG PIC 8
+const destPic = document.querySelector(".content-destination img")
+destPic.addEventListener('click', ()=>{
+    destPic.src="https://images-na.ssl-images-amazon.com/images/I/718vecXok%2BL.jpg"
+})
+
 
 // -------------
+// NEST
+document.querySelector("#link1").addEventListener('click', function(x){
+
+    alert("MESSAGE IN HERE");
+    x.stopPropagation();
+})
+
+document.querySelector(".nav").addEventListener('click', function(){
+    document.querySelector(".nav").style.backgroundColor = "pink";
+})
+
+// PREVENT DEFAULT
+
+// const nav = document.querySelector(".nav-link");
+// // console.log(nav);
+// nav.forEach(element => element.preventDefault());
 
 
-
+const nav = document.querySelectorAll('.nav-link');
+nav.forEach(element => element.addEventListener('click', event => event.preventDefault()));
