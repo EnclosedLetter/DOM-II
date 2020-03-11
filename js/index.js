b47d39b9cc3b753283bbfa5d87fc31e977f2b02c
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-11 06:17:10 -0700

    set up mouseenter

--- js/index.js
+++ js/index.js
@@ -7,21 +7,47 @@
 //     console.log('the nav got clicked');
 // })
 
-// BUS PIC 
-const busPic = document.querySelector(".intro").addEventListener("dblclick", function(){
-    document.getElementById("fBus").style.width = "75%";
-})
 
 //BUS TEXT
 const busText = document.querySelector(".nav-container").addEventListener("mouseover", function(){
     document.querySelector(".logo-heading").textContent = "WELCOME TO THE FUN BUS!";
 })
 
-// LET'S GO Text
-const adventurePic = document.querySelector(".img-content").addEventListener("dblclick", function(){
-    document.querySelector("img").textContent = "BOO!";
+// BUS PIC 
+const busPic = document.querySelector(".intro").addEventListener("dblclick", function(){
+    document.getElementById("fBus").style.width = "75%";
 })
 
-// const busPic = document.querySelector(".intro").addEventListener("mouseover", function(){
-//     document.getElementById("fBus").style.width = "50%";
-// })
+// LET'S GO Text
+const letsgoText = document.querySelector(".text-content").addEventListener("dblclick", function(){
+    document.querySelector(".text-content h2").style.color = "purple";
+})
+
+// YOU CAN PUT MULTIPLE TAGS IN QUERY SELECTOR
+
+// ADVENTURE pic
+const adventurePic = document.querySelector(".img-content").addEventListener("dblclick", function(){
+    document.querySelector("#advPic").style.width = "50%";
+})
+
+const boatPic = document.querySelector(".content-section .img-fluid-rounded");
+boatPic.addEventListener('mouseenter', ()=>{
+    boatPic.style.transform = "scale(1.2)";
+    adventPic.style.transition = "all 0.3s";
+});
+
+boatPic.addEventListener("mouseleave", () => {
+    boatPic.style.transform = "scale(1)";
+});
+
+// const adventPic = document.querySelector(".img-content");
+// // console.log(catImage);
+
+// adventPic.addEventListener("mouseenter", () => {
+//     adventPic.style.transform = "scale(1.2)";
+//     adventPic.style.transition = "all 0.3s";
+//    })
+
+//    adventPic.addEventListener("mouseleave", () => {
+//     adventPic.style.transform = "scale(1)";
+//    })
